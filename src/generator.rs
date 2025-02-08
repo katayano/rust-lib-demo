bb75f5d6f4150ade9aa09d033e5dd6944af97030
katayama <kata.127.yama@gmail.com>
2025-02-08 23:21:46 +0900

    modify method

--- src/generator.rs
+++ src/generator.rs
@@ -1,7 +1,7 @@
 use rand::Rng;
 
 pub fn gen_ran() -> u8 {
-    let mut rng = rand::thread_rng(); // 乱数生成器を作成
-    let n: u8 = rng.gen(); // 乱数を生成
+    let mut rng = rand::rng(); // 乱数生成器を作成
+    let n: u8 = rng.random(); // 乱数を生成
     n
 }
